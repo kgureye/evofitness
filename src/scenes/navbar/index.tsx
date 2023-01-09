c28a5c5c8584b3c59e6fe962304eb64a6dfc22b6
kgureye <kanigureye@outlook.com>
2023-01-09 22:10:19 +0100

    added enums

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import Logo from "@/assets/logo.png";
 import Link from "./Link";
-
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px")
 
   return (
   <nav>
@@ -21,7 +25,8 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           <img src={Logo} alt="logo" />
 
           {/* RIGHT SIDE */}
-          <div className={`${flexBetween} w-full`}>
+          {isAboveMediumScreens ? (
+            <div className={`${flexBetween} w-full`}>
             <div className={`${flexBetween} gap-8 text-sm`}>
               <Link 
               page="Home"
@@ -46,13 +51,32 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <div className={`${flexBetween} gap-8`}>
               {/* Sign in & Become a member button */}
               <p>Sign in</p>
-              <button>Become a Member</button>
+              <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
             </div>
           </div>
         </div>
+        ) : (
+          <button
+          className="rounded-full bg-secondary-500 p-2"
+          onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+          <Bars3Icon className="w-6 h-6 text-white" />
+          </button>
+      )}
        </div>
      </div>
     </div>
+
+   {/* MOBILE MENU  */}
+
+   {!isAboveMediumScreens && isMenuToggled && (
+    <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        {/* CLOSE ICON */}
+        <div className="flex justify-end p-12">
+            
+        </div>
+    </div>
+   )}
   </nav>
   );
 }
