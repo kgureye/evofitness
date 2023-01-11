20ac1fc3aa94c1796b682dc28ffb52337594fb84
kgureye <kanigureye@outlook.com>
2023-01-11 19:12:33 +0100

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Navbar from  "@/scenes/navbar";
 import Home from  "@/scenes/home";
 import Benefits from "@/scenes/benefits"
-import ContactUS from "@/scenes/contactUs"
+import ContactUS from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 import OurClasses from "./scenes/ourClasses";
@@ -36,6 +37,7 @@ function App() {
      <Benefits setSelectedPage={setSelectedPage} />
      <OurClasses setSelectedPage={setSelectedPage} />
      <ContactUS setSelectedPage={setSelectedPage} />
+     <Footer />
     </div>
   );
 }
