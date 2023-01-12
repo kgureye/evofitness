182bc9a15ab347f6c3c31fe2b5c586a08643c066
kgureye <kanigureye@outlook.com>
2023-01-12 14:56:38 +0100

    typo fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,44 @@
-import Navbar from  "@/scenes/navbar";
-import Home from  "@/scenes/home";
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
-import ContactUS from "@/scenes/contactUs";
+import ContactUs from "@/scenes/contactUs";
 import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
-import OurClasses from "./scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
-    );
-    
-    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-    /* red color on home whenever we're at top.  */
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY === 0) {
-          setIsTopOfPage(true);
-          setSelectedPage(SelectedPage.Home); 
-        } else if(window.scrollY !== 0) {
-          setIsTopOfPage(false);
-        }
-      };
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll); // Remove scroll as well as function when they leave the page. Useful for preventing bugs and other memory leaks.
-      };
-    }, [])
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-  <div className="app bg-gray-20">
-     <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-     <Home selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-     <Benefits setSelectedPage={setSelectedPage} />
-     <OurClasses setSelectedPage={setSelectedPage} />
-     <ContactUS setSelectedPage={setSelectedPage} />
-     <Footer />
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
 
-export default App
-
+export default App;
